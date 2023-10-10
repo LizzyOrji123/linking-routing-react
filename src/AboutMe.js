9a7fa7ed203d32ef7a1711e9147c73870c9ef114
LizzyOrji123 <cegscutie4rill@yahoo.com>
2023-10-10 19:27:15 +0100

    created Navbar

---
+++ src/AboutMe.js
@@ -0,0 +1,7 @@
+function AboutMe() {
+  return(
+    <h1>About our page</h1>
+  );
+};
+
+export default AboutMe;
--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
+import Homepage from './Homepage.js';
+import AboutMe from './AboutMe.js';
+import Contact from './Contact.js';
 import './App.css';
+import { Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav className="Navbar">
+        <Link to='/' className='Nav-item'>Homepage</Link>
+        <Link to='/About me' className='Nav-item'>About Me</Link>
+        <Link to='/Contact' className='Nav-item'>Contact Us</Link>
+      </nav>
+      <Routes>
+        <Route path='/' element={<Homepage/>}/>
+        <Route path='About me' element={<AboutMe/>}/>
+        <Route path='Contact' element={<Contact/>}/>
+      </Routes>
     </div>
   );
-}
+};
 
 export default App;
